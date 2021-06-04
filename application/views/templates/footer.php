5d248bd5f4b7f7c624bd45bc0baa3892c0c03545
dodinovembri <dodinovembri32gmail.com>
2021-06-04 08:20:30 +0700

    update

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -17,67 +17,65 @@
   <!-- ./wrapper -->
 
   <!-- jQuery -->
-  <script src="<?php echo base_url() ?>assets/plugins/jquery/jquery.min.js"></script>
+  <script src="<?php echo base_url('assets/plugins/jquery/jquery.min.js') ?>"></script>
   <!-- jQuery UI 1.11.4 -->
-  <script src="<?php echo base_url() ?>assets/plugins/jquery-ui/jquery-ui.min.js"></script>
+  <script src="<?php echo base_url('assets/plugins/jquery-ui/jquery-ui.min.js') ?>"></script>
   <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
   <script>
     $.widget.bridge('uibutton', $.ui.button)
   </script>
   <!-- Bootstrap 4 -->
-  <script src="<?php echo base_url() ?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
   <!-- ChartJS -->
-  <script src="<?php echo base_url() ?>assets/plugins/chart.js/Chart.min.js"></script>
+  <script src="<?php echo base_url('assets/plugins/chart.js/Chart.min.js') ?>"></script>
   <!-- Sparkline -->
-  <script src="<?php echo base_url() ?>assets/plugins/sparklines/sparkline.js"></script>
+  <script src="<?php echo base_url('assets/plugins/sparklines/sparkline.js') ?>"></script>
   <!-- JQVMap -->
-  <script src="<?php echo base_url() ?>assets/plugins/jqvmap/jquery.vmap.min.js"></script>
-  <script src="<?php echo base_url() ?>assets/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+  <script src="<?php echo base_url('assets/plugins/jqvmap/jquery.vmap.min.js') ?>"></script>
+  <script src="<?php echo base_url('assets/plugins/jqvmap/maps/jquery.vmap.usa.js') ?>"></script>
   <!-- jQuery Knob Chart -->
-  <script src="<?php echo base_url() ?>assets/plugins/jquery-knob/jquery.knob.min.js"></script>
+  <script src="<?php echo base_url('assets/plugins/jquery-knob/jquery.knob.min.js') ?>"></script>
   <!-- daterangepicker -->
-  <script src="<?php echo base_url() ?>assets/plugins/moment/moment.min.js"></script>
-  <script src="<?php echo base_url() ?>assets/plugins/daterangepicker/daterangepicker.js"></script>
+  <script src="<?php echo base_url('assets/plugins/moment/moment.min.js') ?>"></script>
+  <script src="<?php echo base_url('assets/plugins/daterangepicker/daterangepicker.js') ?>"></script>
   <!-- Tempusdominus Bootstrap 4 -->
-  <script src="<?php echo base_url() ?>assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+  <script src="<?php echo base_url('assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') ?>"></script>
   <!-- Summernote -->
-  <script src="<?php echo base_url() ?>assets/plugins/summernote/summernote-bs4.min.js"></script>
+  <script src="<?php echo base_url('assets/plugins/summernote/summernote-bs4.min.js') ?>"></script>
   <!-- overlayScrollbars -->
-  <script src="<?php echo base_url() ?>assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+  <script src="<?php echo base_url('assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') ?>"></script>
   <!-- AdminLTE App -->
-  <script src="<?php echo base_url() ?>assets/dist/js/adminlte.js"></script>
+  <script src="<?php echo base_url('assets/dist/js/adminlte.js') ?>"></script>
   <!-- AdminLTE for demo purposes -->
-  <script src="<?php echo base_url() ?>assets/dist/js/demo.js"></script>
+  <script src="<?php echo base_url('assets/dist/js/demo.js') ?>"></script>
   <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-  <script src="<?php echo base_url() ?>assets/dist/js/pages/dashboard.js"></script>
+  <script src="<?php echo base_url('assets/dist/js/pages/dashboard.js') ?>"></script>
   <!-- Select2 -->
-  <script src="<?php echo base_url() ?>assets/plugins/select2/js/select2.full.min.js"></script>
-  <!-- Bootstrap4 Duallistbox -->
-  <script src="<?php echo base_url() ?>assets/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
+  <script src="<?php echo base_url('assets/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js') ?>"></script>
   <!-- InputMask -->
-  <script src="<?php echo base_url() ?>assets/plugins/moment/moment.min.js"></script>
-  <script src="<?php echo base_url() ?>assets/plugins/inputmask/jquery.inputmask.min.js"></script>
+  <script src="<?php echo base_url('assets/plugins/moment/moment.min.js') ?>"></script>
+  <script src="<?php echo base_url('assets/plugins/inputmask/jquery.inputmask.min.js') ?>"></script>
   <!-- Bootstrap Switch -->
-  <script src="<?php echo base_url() ?>assets/plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
+  <script src="<?php echo base_url('assets/plugins/bootstrap-switch/js/bootstrap-switch.min.js') ?>"></script>
   <!-- BS-Stepper -->
-  <script src="<?php echo base_url() ?>assets/plugins/bs-stepper/js/bs-stepper.min.js"></script>
+  <script src="<?php echo base_url('assets/plugins/bs-stepper/js/bs-stepper.min.js') ?>"></script>
   <!-- dropzonejs -->
-  <script src="<?php echo base_url() ?>assets/plugins/dropzone/min/dropzone.min.js"></script>
+  <script src="<?php echo base_url('assets/plugins/dropzone/min/dropzone.min.js') ?>"></script>
   <!-- DataTables  & Plugins -->
-  <script src="<?php echo base_url() ?>assets/plugins/datatables/jquery.dataTables.min.js"></script>
-  <script src="<?php echo base_url() ?>assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-  <script src="<?php echo base_url() ?>assets/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-  <script src="<?php echo base_url() ?>assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-  <script src="<?php echo base_url() ?>assets/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-  <script src="<?php echo base_url() ?>assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-  <script src="<?php echo base_url() ?>assets/plugins/jszip/jszip.min.js"></script>
-  <script src="<?php echo base_url() ?>assets/plugins/pdfmake/pdfmake.min.js"></script>
-  <script src="<?php echo base_url() ?>assets/plugins/pdfmake/vfs_fonts.js"></script>
-  <script src="<?php echo base_url() ?>assets/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-  <script src="<?php echo base_url() ?>assets/plugins/datatables-buttons/js/buttons.print.min.js"></script>
-  <script src="<?php echo base_url() ?>assets/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+  <script src="<?php echo base_url('assets/plugins/datatables/jquery.dataTables.min.js') ?>"></script>
+  <script src="<?php echo base_url('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') ?>"></script>
+  <script src="<?php echo base_url('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js') ?>"></script>
+  <script src="<?php echo base_url('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') ?>"></script>
+  <script src="<?php echo base_url('assets/plugins/datatables-buttons/js/dataTables.buttons.min.js') ?>"></script>
+  <script src="<?php echo base_url('assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') ?>"></script>
+  <script src="<?php echo base_url('assets/plugins/jszip/jszip.min.js') ?>"></script>
+  <script src="<?php echo base_url('assets/plugins/pdfmake/pdfmake.min.js') ?>"></script>
+  <script src="<?php echo base_url('assets/plugins/pdfmake/vfs_fonts.js') ?>"></script>
+  <script src="<?php echo base_url('assets/plugins/datatables-buttons/js/buttons.html5.min.js') ?>"></script>
+  <script src="<?php echo base_url('assets/plugins/datatables-buttons/js/buttons.print.min.js') ?>"></script>
+  <script src="<?php echo base_url('assets/plugins/datatables-buttons/js/buttons.colVis.min.js') ?>"></script>
 
-  <script src="<?php echo base_url() ?>assets/plugins/select2/js/select2.full.min.js"></script>
+  <script src="<?php echo base_url('assets/plugins/select2/js/select2.full.min.js') ?>"></script>
   </body>
 
   </html>
